038cd410b05b857e8df7de323922d95d06b84c70
PEDRO_ESPARZA <260210pedro@gmail.com>
2017-02-27 15:24:18 -0600

    first commit

---
+++ myapp/app/controllers/controller1_controller.rb
@@ -0,0 +1,2 @@
+class Controller1Controller < ApplicationController
+end
---
+++ myapp/test/controllers/controller1_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Controller1ControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
